a93a905ab2708131297b0d451516fe90f35bd989
nikhisable5050 <nikhilsable5050@gmail.com>
2026-01-06 15:22:16 +0530

    refactor: add Lombok annotations to Plan entity

--- lovable-clone/src/main/java/com/nikhilsable/projects/lovable_clone/entity/Plan.java
+++ lovable-clone/src/main/java/com/nikhilsable/projects/lovable_clone/entity/Plan.java
@@ -1,5 +1,13 @@
 package com.nikhilsable.projects.lovable_clone.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Plan {
     Long id;
 
